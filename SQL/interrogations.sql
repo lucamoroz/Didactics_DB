aa7887256d520b277b05a3f64b57775608e6f6a2
Francesco Pham <pham.francesco@gmail.com>
2019-03-19 14:21:29 +0100

    aggiunta nome e cognome del responsabile all'interrogazione

--- SQL/interrogations.sql
+++ SQL/interrogations.sql
@@ -4,13 +4,15 @@ FROM propone as p JOIN attivita_formativa as a ON p.attivita_formativa = a.codic
 WHERE p.coorte = 2015 and p.corso_laurea = 'IN0508'
 ORDER BY p.anno ASC, p.semestre ASC;
 
--- Dati un 'corso di studi' e una 'coorte': trovare codice, nome, anno accademico e responsabile di tutte le attività formative attivate ordinate per anno
-SELECT af.codice, af.nome, i.anno_accademico, i.responsabile
+-- Dati un 'corso di studi' e una 'coorte': trovare le attività formative attivate ordinate per anno
+-- mostrando codice, nome, canale e anno accademico dell'istanza attività formativa e nome + cognome del responsabile
+SELECT af.codice, af.nome, i.canale, i.anno_accademico, CONCAT(d.nome, ' ', d.cognome) as responsabile
 FROM attiva as a JOIN istanza_attivita_formativa as i 
     ON a.attivita_formativa = i.attivita_formativa 
         AND a.canale = i.canale 
         AND a.anno_accademico = i.anno_accademico 
         AND a.responsabile = i.responsabile
 JOIN attivita_formativa as af ON i.attivita_formativa = af.codice
+JOIN docente as d ON i.responsabile = d.matricola
 WHERE a.coorte = 2015 and a.corso_laurea = 'IN0508'
 ORDER BY i.anno_accademico ASC;

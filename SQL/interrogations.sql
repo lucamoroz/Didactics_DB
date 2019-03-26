67fd5f55c763db532278e50528b89cb2974760be
Francesco Pham <pham.francesco@gmail.com>
2019-03-26 11:51:11 +0100

    Removed a query because already included in the second one

--- SQL/interrogations.sql
+++ SQL/interrogations.sql
@@ -18,19 +18,6 @@ LEFT JOIN docente as d ON a.responsabile = d.matricola
 WHERE p.coorte = 2015 and p.corso_laurea = 'IN0508'
 ORDER BY p.anno ASC, p.semestre ASC;
 
--- Dati un 'corso di studi' e una 'coorte': trovare le attività formative attivate ordinate per anno
--- mostrando codice, nome, canale e anno accademico dell'istanza attività formativa e nome + cognome del responsabile
-SELECT af.codice, af.nome, i.canale, i.anno_accademico, CONCAT(d.nome, ' ', d.cognome) as responsabile
-FROM attiva as a JOIN istanza_attivita_formativa as i 
-    ON a.attivita_formativa = i.attivita_formativa 
-        AND a.canale = i.canale 
-        AND a.anno_accademico = i.anno_accademico 
-        AND a.responsabile = i.responsabile
-JOIN attivita_formativa as af ON i.attivita_formativa = af.codice
-JOIN docente as d ON i.responsabile = d.matricola
-WHERE a.coorte = 2015 and a.corso_laurea = 'IN0508'
-ORDER BY i.anno_accademico ASC;
-
 -- Dati il codice di una attivita' formativa, il canale, l'anno accademico e il responsabile trovare i dettagli dell'attivita' formativa
 SELECT af.nome as nome, iaf.canale as canale, iaf.anno_accademico as anno_accademico, CONCAT(d.nome, ' ', d.cognome) as responsabile, iaf.acquisire as acquisire, iaf.contenuti as contenuti, iaf.testi as testi
 FROM istanza_attivita_formativa as iaf JOIN attivita_formativa as af
